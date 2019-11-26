36c7fdba7b09b7ec627cc623e806b0a06e471059
Unmesh <unmesh.joshi126@gmail.com>
2019-11-26 16:56:31 +0100

    working lstm

--- lstm.py
+++ lstm.py
@@ -8,44 +8,21 @@ percent_test = 10
 TOPK = 10
 DIM = 200
 N_QUERIES = 500
-
 # each query has topK answers (sequence of 50 answers)
 # each answer has 200 features
-
 # 1 for score, 1 for rank, 3 for (h,r,t) ids and 1 for answer entity
 N_FEATURES = DIM + 6
 
-#N_TOTAL = 100#len()
-#N_TEST = int(N_TOTAL * percent_test / 100)
-#N_TRAIN = int(N_TOTAL - N_TEST)
-
-#print(N_TRAIN)
-#print(N_FEATURES)
-
 x_train = np.random.random((N_QUERIES, TOPK, N_FEATURES))
-#data = [[i for i in range(100)]]
-#data = np.array(x_train, dtype=float)
-
-y_train = ((np.random.random(N_QUERIES) * 42) % 2).astype(int)
-#target = np.array(y_train, dtype=int)
-
-#data = data.reshape((1, N_TRAIN, N_FEATURES))
-#target = target.reshape((1, N_TRAIN, 1))
-
+y_train = ((np.random.random((N_QUERIES, TOPK, 1)) * 42) % 2).astype(int)
 
 x_test = np.random.random((20, TOPK, N_FEATURES))
-#x_test=[i for i in range(100,200)]
-#x_test=np.array(x_test).reshape((1, N_TEST, N_FEATURES));
-
-y_test = ((np.random.random(20) * 42) % 2).astype(int)
-#y_test=np.array(y_test).reshape(1, N_TEST, 1)
-
+y_test = ((np.random.random((20, TOPK, 1)) * 42) % 2).astype(int)
 
 model = Sequential()
-model.add(LSTM(1, input_shape=(TOPK, N_FEATURES),return_sequences=False))
-#model.add(Dense((N_FEATURES,N_TRAIN)))
+model.add(LSTM(1, input_shape=(TOPK, N_FEATURES),return_sequences=True))
+
 model.compile(loss='mean_absolute_error', optimizer='adam',metrics=['accuracy'])
+model.summary()
 model.fit(x_train, y_train, epochs=100, batch_size=1, verbose=2,validation_data=(x_test, y_test))
-
-
-predict = model.predict(data)
+predict = model.predict(x_test)
--- svm.py
+++ svm.py
@@ -1,53 +1,48 @@
 import numpy as np
-
-#from keras.models import Sequential
-#from keras.layers import Dense
-#from keras.layers import LSTM
-
+import sys
+import json
 from sklearn.svm import SVC
 
-percent_test = 10
-TOPK = 10
-DIM = 200
+input_file = sys.argv[1]
 
-# 1 for score, 1 for rank, 3 for (h,r,t) ids and 1 for answer entity
-N_FEATURES = DIM + 6
-N_TOTAL = 100#len()
+print("Loading training data...", end = " ")
+with open(input_file, "r") as fin:
+    data = json.loads(fin.read())
+
+print("DONE")
+all_data_x = data['x_tail']
+all_data_y = data['y_tail']
+
+print("Splitting data...", end = " ")
+percent_test = 10
+N_TOTAL = len(all_data_x)
 N_TEST = int(N_TOTAL * percent_test / 100)
 N_TRAIN = int(N_TOTAL - N_TEST)
-
+print(N_TOTAL)
 print(N_TRAIN)
-print(N_FEATURES)
-x_train_shape = (N_TRAIN, N_FEATURES)
-y_train_shape = (N_TRAIN, 1)
+print(N_TEST)
+print("#"*80)
 
-x_train = np.random.random((N_TRAIN, N_FEATURES))
-#data = [[i for i in range(100)]]
-#data = np.array(x_train, dtype=float)
+x_train = all_data_x[:N_TRAIN]
+y_train = all_data_y[:N_TRAIN]
 
-y_train = ((np.random.random(N_TRAIN) * 42) % 2).astype(int)
-#target = np.array(y_train, dtype=int)
+labels = np.unique(y_train)
+print("yrai[0], ", y_train[0])
 
-#data = data.reshape((1, N_TRAIN, N_FEATURES))
-#target = target.reshape((1, N_TRAIN, 1))
+print("Training : ", len(x_train))
+x_test = all_data_x[N_TRAIN:]
+y_test = all_data_y[N_TRAIN:]
 
+print("Test ", len(x_test))
 
-x_test = np.random.random((N_TEST, N_FEATURES))
-#x_test=[i for i in range(100,200)]
-#x_test=np.array(x_test).reshape((1, N_TEST, N_FEATURES));
-
-y_test = ((np.random.random(N_TEST) * 42) % 2).astype(int)
-#y_test=np.array(y_test).reshape(1, N_TEST, 1)
-
+print("*** ", labels)
+print("DONE")
 model = SVC()
+print("Fitting SVM...", end = " ")
 model.fit(x_train, y_train)
 
+print("DONE")
 print(model.score(x_test, y_test))
-#model = Sequential()
-#model.add(LSTM(N_TRAIN, input_shape=(N_FEATURES,),return_sequences=False))
-#model.add(Dense((N_FEATURES,N_TRAIN)))
-#model.compile(loss='mean_absolute_error', optimizer='adam',metrics=['accuracy'])
-#model.fit(data, target, nb_epoch=100, batch_size=1, verbose=2,validation_data=(x_test, y_test))
 
 a,b = (model.predict(x_test) == y_test).sum() , len(y_test)
 print(a, b)
