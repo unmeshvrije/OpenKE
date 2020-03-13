57619753ceec8b0a44ddae953820db3d629484db
Unmesh <unmesh.joshi126@gmail.com>
2020-03-13 17:14:42 +0100

    implement dynamic topk for subgraphs

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from subgraph_classifier import SubgraphClassifier
 def parse_args():
     parser = argparse.ArgumentParser(description = 'Read training/test file and run LSTM training or test.')
     parser.add_argument('--testfile', dest ='test_file', required = True, type = str, help = 'File containing test data.')
+    parser.add_argument('--trainfile', dest ='train_file', required = True, type = str, help = 'File containing test data.')
     parser.add_argument('--modelfile', dest ='model_file',type = str, help = 'File containing test data.')
     parser.add_argument('--classifier', dest ='classifier',type = str, help = 'Classifier.')
     parser.add_argument('--weightsfile', dest ='weights_file', type = str, help = 'File containing test data.')
@@ -16,6 +17,7 @@ def parse_args():
     parser.add_argument('-rd', '--result-dir', dest ='result_dir', type = str, default = "/var/scratch2/uji300/OpenKE-results/",help = 'Output dir.')
     parser.add_argument('--topk', dest = 'topk', required = True, type = int, default = 10)
     parser.add_argument('--db', required = True, dest = 'db', type = str, default = None)
+    parser.add_argument('--model', dest ='model',type = str, default = "transe", help = 'Embedding model name.')
     parser.add_argument('--pred', dest ='pred', type = str, choices = ['head', 'tail'], help = 'Prediction type (head/tail)')
     return parser.parse_args()
 
@@ -34,6 +36,8 @@ elif args.classifier == "sub":
     emb_file = args.emb_file
     sub_file = args.sub_file
     subemb_file = args.subemb_file
-    mys = SubgraphClassifier(args.pred, args.topk, queries_file_path, emb_file, sub_file, subemb_file)
+    mys = SubgraphClassifier(args.pred, args.topk, queries_file_path, emb_file, sub_file, subemb_file, args.model, args.train_file)
     mys.predict()
     mys.results()
+    print("fil 1s = {}, 0s = {}".format(mys.y_predicted_fil.count(1), mys.y_predicted_fil.count(0)))
+    print("raw 1s = {}, 0s = {}".format(mys.y_predicted_raw.count(1), mys.y_predicted_raw.count(0)))
--- subgraph_classifier.py
+++ subgraph_classifier.py
@@ -6,21 +6,33 @@ from answer_classifier import AnswerClassifier
 from subgraphs import Subgraph
 from subgraphs import SUBTYPE
 from numpy import linalg as LA
+from subgraphs import read_triples
 
 class SubgraphClassifier(AnswerClassifier):
 
-    def __init__(self, type_prediction, topk, queries_file_path, embeddings_file_path, subgraphs_file_path, sub_emb_file_path, model_str):
+    def __init__(self, type_prediction, topk, queries_file_path, embeddings_file_path, subgraphs_file_path, sub_emb_file_path, model_str, training_file_path):
         super(SubgraphClassifier, self).__init__(type_prediction, queries_file_path)
         self.topk = topk
         self.emb_file_path = embeddings_file_path
         self.sub_file_path = subgraphs_file_path
         self.sub_emb_file_path = sub_emb_file_path
+        self.training_file_path = training_file_path
         self.init_embeddings()
         self.init_subgraphs()
         self.init_sub_embeddings()
+        self.init_training_triples()
         self.init_model_score_function(model_str)
 
-    def init_model_score_function(model_str):
+    def init_training_triples(self):
+        triples = read_triples(self.training_file_path)
+        # triples are in the form (h,r,t)
+        # For type_prediction : head, we sort by tail
+        if self.type_prediction == "head":
+            self.training_triples = sorted(triples, key = lambda l : (l[2], l[1]))
+        elif self.type_prediction == "tail":
+            self.training_triples = sorted(triples, key = lambda l : (l[2], l[0]))
+
+    def init_model_score_function(self, model_str):
         if model_str == "transe":
             self.model_score = self.transe_score
 
@@ -53,9 +65,47 @@ class SubgraphClassifier(AnswerClassifier):
         self.predict_internal(self.x_test_raw, self.y_predicted_raw)
         self.predict_internal(self.x_test_fil, self.y_predicted_fil)
 
+    def get_dynamic_topk(self, ent, rel, sub_indexes):
+        '''
+            1. Search ent, rel in training triples
+            2. If answer is found, look for the answer in sorted subgraphs
+        '''
+        print("ent {}, rel {} ". format(ent, rel))
+        answers = []
+        for index, triple in enumerate(self.training_triples):
+            if triple[2] != rel:
+                continue
+
+            if triple[2] > rel:
+                break
+
+            if self.type_prediction == "head":
+                if triple[1] == ent:
+                    answers.append(triple[0])
+            elif self.type_prediction == "tail":
+                if triple[0] == ent:
+                    answers.append(triple[1])
+
+        print(answers)
+        if len(answers) == 0:
+            return int(0.1 * len(sub_indexes))
+
+        found_index = []
+        for answer in answers:
+            for j, sub_index in enumerate(sub_indexes):
+                if answer in self.subgraphs[sub_index].data['entities']:
+                    found_index.append(j)
+                    break
+        if len(found_index) == 0:
+            return int(0.1 * len(sub_indexes))
+
+        print("found topk : ", found_index)
+        return max(found_index)
+
+
     def predict_internal(self, x_test, y_predicted):
         # Go over all test queries
-        for index in range(0, len(x_test), self.topk):
+        for index in tqdm(range(0, len(x_test), self.topk)):
             #print(index , " : ")
             features = np.array(x_test[index: index + self.topk])
             ent = int(features[0][0])
@@ -63,13 +113,18 @@ class SubgraphClassifier(AnswerClassifier):
             topk_ans_entities = features[:, 2].astype(int)
 
             subgraph_scores = []
-            for se in self.S:
-                subgraph_scores.append(self.get_subgraph_scores(se, self.E[ent], self.R[rel],  self.type_prediction, self.model_score))
+            for index, se in enumerate(self.S):
+                if self.subgraphs[index].data['ent'] == ent and self.subgraphs[index].data['rel'] == rel:
+                    subgraph_scores.append(np.inf)
+                else:
+                    subgraph_scores.append(self.get_subgraph_scores(se, self.E[ent], self.R[rel],  self.type_prediction, self.model_score))
             sub_indexes = np.argsort(subgraph_scores)
 
             # TODO: Computer dynamic topk for query (ent, rel, ?)
             # and only check the answer in those subgraphs (sub_indexes)
-            topk_subgraphs = int(0.1 * len(sub_indexes))
+            #topk_subgraphs = int(0.1 * len(sub_indexes))
+            topk_subgraphs = self.get_dynamic_topk(ent, rel, sub_indexes)
+
             for i, answer in enumerate(topk_ans_entities):
                 cnt_presence_in_sub = 0;
                 # Check in only topK subgraphs
@@ -77,7 +132,8 @@ class SubgraphClassifier(AnswerClassifier):
                     if answer in self.subgraphs[sub_index].data['entities']:
                         cnt_presence_in_sub += 1
                         #print("{} FOUND in subgraph # {}". format(answer, j))
-                if cnt_presence_in_sub != 0:
+                #if cnt_presence_in_sub != 0:
+                if cnt_presence_in_sub > 10: #topk_subgraphs/2:
                     y_predicted.append(1)
                 else:
                     y_predicted.append(0)
