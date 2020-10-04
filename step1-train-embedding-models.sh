b70c440d422bbe6f3c75354d3f0c96f5d98a4693
Unmesh <unmesh.joshi126@gmail.com>
2020-10-05 01:42:22 +0200

    script cleanup and renaming

---
+++ step1-train-embedding-models.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+source ~/.bashrc
+
+if [ "$#" -ne 3 ];
+then
+    echo "usage: $0 [result_dir] [model] [db]. Found $#"
+    exit
+fi
+
+RD=$1 #"/var/scratch2/uji300/OpenKE-results/"
+E=$2  # "transe", "hole", "rotate"
+DB=$3 #"fb15k237"
+RDB=$RD"$DB/"
+RDE=$RDB"embeddings/" # contains file in the name format : db-model.json
+RDD=$RDB"data/" # contains files in the name format fb15k237-transe-training-topk-10.pkl (embedding features) or .json (raw answers)
+RDM=$RDB"models/"
+RDS=$RDB"subgraphs/" # contains files in the name format fb15k237-transe-{subgraphs/avgemb/varemb}-tau-10.pkl
+
+
+EMB_MODEL_PATH="$RDE""$DB-$E.json"
+echo "$EMB_MODEL_PATH"
+if [ ! -f $EMB_MODEL_PATH ];
+then
+    python embedding_model.py --gpu --db $DB --mode "train" --model $E
+fi
+
+echo "Model $EMB_MODEL_PATH is found. Generating test answers..."
+for K in 10 #1 3 5
+do
+    python embedding_model.py --gpu --db $DB --mode "trainAsTest" --model $E --topk $K
+    python embedding_model.py --gpu --db $DB --mode "test" --model $E --topk $K
+done
---
+++ step4-train-lstm-mlp-classifiers.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+if [ "$#" -ne 3 ];
+then
+    echo "usage: $0 [result_dir] [model] [db]. Found $#"
+    exit
+fi
+
+# Result Directory
+RD=$1 # "/var/scratch2/uji300/OpenKE-results/"
+E=$2
+DB=$3
+RDB=$RD"$DB/"
+RDE=$RDB"embeddings/" # contains file in the name format : db-model.json
+RDD=$RDB"data/" # contains files in the name format fb15k237-transe-training-topk-10.pkl (embedding features) or .json (raw answers)
+RDM=$RDB"models/"
+#E="transe"
+M="lstm"
+U=100
+DR=0.2
+
+for K in 100 # 1 3 5
+do
+    for P in "head" "tail"
+    do
+    for M in "lstm" "mlp"
+    do
+    for U in 100 #200 #500
+    do
+    for DR in 0.2 #0.5 0.8
+    do
+    mo_file=$RDM"$DB-$E-training-topk-$K-$P-model-$M-units-$U-dropout-$DR.json"
+    if [ ! -f  $mo_file ];
+    then
+        echo "$mo_file not found. Generating one...";
+        python train_answer_model.py --infile $RDD"$DB-$E-training-topk-$K.pkl" --topk $K --mode train  --pred $P --db $DB --units $U --dropout $DR --model $M
+        echo "DONE"
+    else
+        echo "$mo_file FOUND";
+    fi
+    done;
+    done;
+    done;
+    done;
+done;
