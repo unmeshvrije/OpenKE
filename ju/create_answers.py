320b1084a55b21b5c09e0a2351d2c5c3baedf6dd
Jacopo Urbani <jacopo@cs.vu.nl>
2020-12-10 15:06:17 +0100

    Added some auxiliary files

---
+++ ju/create_answers.py
@@ -0,0 +1,109 @@
+import torch
+import kge.model
+import json
+import os
+import argparse
+from tqdm import tqdm
+
+def parse_args():
+    parser = argparse.ArgumentParser(description = '')
+    parser.add_argument('--known_answers_train_file', dest='known_answers_train_file', type=str)
+    parser.add_argument('--known_answers_valid_file', dest='known_answers_valid_file', type=str)
+    parser.add_argument('--result_dir', dest ='result_dir', type = str, help = 'Output dir.')
+    parser.add_argument('--topk', dest = 'topk', type = int, default = 10)
+    parser.add_argument('--db', dest = 'db', type = str, default = "fb15k237")
+    parser.add_argument('--mode', dest = 'mode', type = str, default = "test", choices = ['train', 'valid', 'test'])
+    parser.add_argument('--model', dest='model', type=str, default="transe", choices=['complex', 'rotate', 'transe'])
+    parser.add_argument('--type_prediction', dest='type_prediction', type=str, default="head", choices=['head', 'tail'])
+    return parser.parse_args()
+
+args = parse_args()
+
+'''
+    0. Load the model
+'''
+model_path = args.result_dir + '/' + args.db + "/embeddings/" + args.db + "-" + args.model + ".pt"
+queries_full_path = args.result_dir + '/' + args.db + '/queries/' + args.db + '-' + args.mode + '-' + args.type_prediction + '.json'
+model = kge.model.KgeModel()
+model.load_from_checkpoint(model_path)
+
+'''
+    1. Setup  out directories
+'''
+answers_dir =  args.result_dir + args.db + "/answers/"
+os.makedirs(answers_dir, exist_ok = True)
+ent_queries = []
+rel_queries = []
+with open(queries_full_path, "rt") as fin:
+    records = json.loads(fin.read())
+    for r in records:
+        ent_queries.append(r['ent'])
+        rel_queries.append(r['rel'])
+        assert(args.type_prediction == 'tail' or r['type'] == 0)
+        assert (args.type_prediction == 'head' or r['type'] == 1)
+
+known_answers = set()
+if args.mode == 'test':
+    with open(args.known_answers_train_file, 'rt') as fin:
+        nfacts = int(fin.readline())
+        for l in fin:
+            tkns = l.split(' ')
+            h = int(tkns[0])
+            t = int(tkns[1])
+            r = int(tkns[2])
+            if args.type_prediction == 'head':
+                known_answers.add((t,r,h))
+            elif args.type_prediction == 'tail':
+                known_answers.add((h,r,t))
+    with open(args.known_answers_valid_file, 'rt') as fin:
+        nfacts = int(fin.readline())
+        for l in fin:
+            tkns = l.split(' ')
+            h = int(tkns[0])
+            t = int(tkns[1])
+            r = int(tkns[2])
+            if args.type_prediction == 'head':
+                known_answers.add((t,r,h))
+            elif args.type_prediction == 'tail':
+                known_answers.add((h,r,t))
+
+'''
+    2. Make the predictions
+'''
+E = model._entity_embedder._embeddings_all()
+R = model._relation_embedder._embeddings_all()
+ent_dim = len(E[0].tolist())
+rel_dim = len(R[0].tolist())
+suf = ""
+if args.mode == "test":
+    suf = "-fil"
+answers_filename = args.db + "-answers-" + args.model + "-" + args.mode + "-" + args.topk + "-" + args.type_predictions + suf + ".pkl"
+
+'''
+    3. predictions
+'''
+topk = args.topk
+e = torch.Tensor(ent_queries).long()
+r = torch.Tensor(rel_queries).long()
+if args.type_prediction == 'tail':
+    scores = model.score_sp(e, r)                                       # scores of all objects for (s,p,?)
+else:
+    scores = model.score_po(r, e)
+o = torch.argsort(scores, dim=-1, descending = True)                # index of highest-scoring objects
+scores_sp_sorted, indices = torch.sort(scores, dim = -1, descending = True)  # sorted scores
+
+for index in tqdm(range(0, len(ent_queries))):
+    ent = e[index].item()
+    rel  = r[index].item()
+    filtered_answers = []
+    if args.mode == "train":
+        for oi in o[index][:topk]:
+            filtered_answers.append(oi.item())
+    else:
+        for oi in o[index]:
+            if (ent, rel, oi.item()) not in known_answers:
+                filtered_answers.append(oi.item())
+                if len(filtered_answers) == topk:
+                    break
+    assert(len(filtered_answers) == topk)
+
---
+++ ju/create_queries.py
@@ -0,0 +1,39 @@
+import argparse
+import json
+
+def parse_args():
+    parser = argparse.ArgumentParser(description = '')
+    parser.add_argument('--test_file', dest='test_file', type=str, help='File containing the triples to be converted into queries.')
+    parser.add_argument('--result_dir', dest ='result_dir', type = str, help = 'Output dir.')
+    parser.add_argument('--db', dest = 'db', type = str, default = "fb15k237")
+    parser.add_argument('--mode', dest = 'mode', type = str, default = "test", choices = ['train', 'valid', 'test'])
+    return parser.parse_args()
+args = parse_args()
+
+queries_head = set()
+queries_tail = set()
+with open(args.test_file, 'rt') as fin:
+    ntriples = int(fin.readline())
+    for l in fin:
+        tkns = l.split(' ')
+        h = int(tkns[0])
+        t = int(tkns[1])
+        r = int(tkns[2])
+        queries_head.add((t, r))
+        queries_tail.add((h, r))
+
+queries_head = list(queries_head)
+queries_tail = list(queries_tail)
+output_dir = args.result_dir + '/' + args.db + '/queries'
+out_file_head = args.db + '-' + args.mode + '-head.json'
+with open(output_dir + '/' + out_file_head, 'wt') as fout:
+    o = []
+    for i, q in enumerate(queries_head):
+        o.append({'type' : 0, 'ent' : q[0], 'rel' : q[1] })
+    json.dump(o, fout)
+out_file_tail = args.db + '-' + args.mode + '-tail.json'
+with open(output_dir + '/' + out_file_tail, 'wt') as fout:
+    o = []
+    for i, q in enumerate(queries_tail):
+        o.append({'type' : 0, 'ent' : q[0], 'rel' : q[1] })
+    json.dump(o, fout)
