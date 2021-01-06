d46dad0d956a8cf0f8a729e65379c654b0126e0a
Jacopo Urbani <jacopo@cs.vu.nl>
2021-01-06 20:09:30 +0100

    changes

---
+++ support/convert_unmesh_goldstandard_new_format.py
@@ -0,0 +1,47 @@
+import json
+import pickle
+from datetime import datetime
+
+base_dir = '/Users/jacopo/Desktop/binary-embeddings/fb15k237/'
+annotations_unmesh_transe_head = '/Users/jacopo/Desktop/material_submission2021-unmesh/data_unmesh/fb15k237/out/fb15k237-transe-annotated-topk-10-head-extended.json'
+annotations_unmesh_transe_tail = '/Users/jacopo/Desktop/material_submission2021-unmesh/data_unmesh/fb15k237/out/fb15k237-transe-annotated-topk-10-tail-extended.json'
+
+annotations_head = json.load(open(annotations_unmesh_transe_head, 'rt'))
+annotations_tail = json.load(open(annotations_unmesh_transe_tail, 'rt'))
+annotations_out = 'misc/annotations_transe_unmesh.json'
+out = []
+for a in annotations_head:
+    ent = a['e']
+    rel = a['r']
+    answers = []
+    for answer in a['answers']:
+        if answer['cor'] == 0:
+            checked = False
+        else:
+            checked = True
+        answers.append({'entity_id' : answer['answer'], 'checked' : checked, 'methods' : ['transe']})
+    query = {'type' : 0, 'ent' : ent, 'rel' : rel}
+    o = {'query' : query, 'annotated_answers' : answers, 'annotator' : 'U', 'date': str(datetime.now())}
+    out.append(o)
+
+for a in annotations_tail:
+    ent = a['e']
+    rel = a['r']
+    answers = []
+    for answer in a['answers']:
+        if answer['cor'] == 0:
+            checked = False
+        else:
+            checked = True
+        answers.append({'entity_id' : answer['answer'], 'checked' : checked, 'methods' : ['transe']})
+    query = {'type' : 1, 'ent' : ent, 'rel' : rel}
+    o = {'query' : query, 'annotated_answers' : answers, 'annotator' : 'U', 'date': str(datetime.now())}
+    out.append(o)
+
+fout = open(base_dir + '/' + annotations_out, 'wt')
+json.dump(out, fout)
+fout.close()
+
+
+
+
