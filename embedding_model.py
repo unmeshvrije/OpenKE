5c2bcffe09ee88c6252f87e5996d5555b8a901ab
Unmesh <unmesh.joshi126@gmail.com>
2021-03-15 16:21:38 +0100

    add distmult

--- embedding_model.py
+++ embedding_model.py
@@ -1,6 +1,6 @@
 import openke
 from openke.config import Trainer, Tester
-from openke.module.model import TransE, ComplEx, HolE, RotatE
+from openke.module.model import TransE, ComplEx, HolE, RotatE, Distmult
 from openke.module.loss import MarginLoss, SigmoidLoss, SoftplusLoss
 from openke.module.strategy import NegativeSampling
 from openke.data import TrainDataLoader, TestDataLoader, TrainingAsTestDataLoader
@@ -97,6 +97,18 @@ def choose_model():
         alpha = 0.5
     elif args.model == "hole":
         model = HolE(ent_tot = train_dataloader.get_ent_tot(),
+                    rel_tot  = train_dataloader.get_rel_tot(),
+                    dim = N_DIM);
+        model_with_loss = NegativeSampling(
+                    model = model,
+                    loss = SoftplusLoss(),
+                    batch_size = train_dataloader.get_batch_size(),
+                    regul_rate = 1.0
+                    )
+        epochs = 1000
+        alpha = 0.5
+    elif args.model == "distmult":
+        model = DistMult(ent_tot = train_dataloader.get_ent_tot(),
                     rel_tot  = train_dataloader.get_rel_tot(),
                     dim = N_DIM);
         model_with_loss = NegativeSampling(
