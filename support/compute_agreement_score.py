a97b5faf2d774072edaca328c1da5cb0764a0743
Jacopo Urbani <jacopo@cs.vu.nl>
2021-11-30 13:21:08 +0100

    improved annotator

--- support/compute_agreement_score.py
+++ support/compute_agreement_score.py
@@ -1,26 +1,36 @@
-#cat /home/jurbani/data2/binary-embeddings/fb15k237/annotations/gold-annotations.json-2021-01-26\ 23\:30\:02.286156  | python -c "import sys, json; print(json.load(sys.stdin)['757']['annotator'])"
 import os
-import sys
+import argparse
 import json
-#dir_path = "/home/jurbani/data2/binary-embeddings/fb15k237/annotations/"
-dir_path = "/home/jurbani/data2/binary-embeddings/dbpedia50/annotations/"
 
-json_files = [f for f in os.listdir(dir_path) if f.endswith('.json')]
-jacopo_query_ids = set()
-unmesh_query_ids = set()
 
-for fil in json_files:
-    print(os.path.join(dir_path,fil))
-    with open(os.path.join(dir_path, fil), encoding='utf-8') as fin:
-        objects = json.load(fin)
-        for key in objects.keys():
-            if objects[key]['annotator'] == 'J':
-                #jacopo_query_ids.add(objects[key]['query']['id'])
-                jacopo_query_ids.add((objects[key]['query']['ent'], objects[key]['query']['rel']))
-            elif objects[key]['annotator'] == 'U':
-                #unmesh_query_ids.add(objects[key]['query']['id'])
-                unmesh_query_ids.add((objects[key]['query']['ent'], objects[key]['query']['rel']))
+def parse_args():
+    parser = argparse.ArgumentParser(description = '')
+    parser.add_argument('--result_dir', dest ='result_dir', required=True, type = str, help = 'Output dir.')
+    parser.add_argument('--db', dest ='db', type = str, default = "fb15k237", choices=['fb15k237', 'dbpedia50'])
+    parser.add_argument('--topk', dest='topk', type=int, default=10)
+    return parser.parse_args()
 
-    print(jacopo_query_ids.intersection(unmesh_query_ids))
-    print(sorted(list(unmesh_query_ids)))
-    print(sorted(list(jacopo_query_ids)))
+args = parse_args()
+topk = 10
+file_path = args.result_dir + '/' + args.db + '/annotations/gold-annotations.json'
+annotators_ids = {}
+
+n_annotations = 0
+with open(file_path, encoding='utf-8') as fin:
+    objects = json.load(fin)
+    for key in objects.keys():
+        annotator = objects[key]['annotator']
+        if annotator not in annotators_ids:
+            annotators_ids[annotator] = set()
+        annotators_ids[annotator].add((objects[key]['query']['ent'], objects[key]['query']['rel']))
+        n_annotations += 1
+
+print("N. annotators", len(annotators_ids))
+print("N. annotations", n_annotations)
+intersection = None
+for annotator, annotations in annotators_ids.items():
+    if intersection is None:
+        intersection = annotations
+    else:
+        intersection = intersection.intersection(annotations)
+print("Size intersection", len(intersection))
