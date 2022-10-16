ec3ea5a00824baa7c4cf7208548ab474d4aad355
Dovydas Vadisius <vadisius.dovydas@gmail.com>
2022-10-16 21:56:37 +0200

    add diamond subgraph type to testing phase

--- test-subgraphs.sh
+++ test-subgraphs.sh
@@ -54,7 +54,7 @@ done
 
 
 # Result Directory
-RD="/var/scratch2/uji300/OpenKE-results/"
+RD="/var/scratch/dvs254/OpenKE-results/"
 
 #for E in "transe" "rotate" "complex"
 #do
@@ -74,7 +74,7 @@ RD="/var/scratch2/uji300/OpenKE-results/"
     sub_emb_dir=$RDS
     test_file="./benchmarks/$DB/test2id.txt"
     train_file="./benchmarks/$DB/train2id.txt"
-    edict_file="/var/scratch2/uji300/OpenKE-results/$DB/misc/$DB-id-to-entity.pkl"
-    rdict_file="/var/scratch2/uji300/OpenKE-results/$DB/misc/$DB-id-to-relation.pkl"
+    edict_file="/var/scratch/dvs254/OpenKE-results/$DB/misc/$DB-id-to-entity.pkl"
+    rdict_file="/var/scratch/dvs254/OpenKE-results/$DB/misc/$DB-id-to-relation.pkl"
     echo "Calling Python script"
     python test_subgraphs.py --testfile $test_file --embfile $emb_file --subfile $sub_file --subembdir $sub_emb_dir --topk $K --db $DB --trainfile $train_file --model $E -stp 0.01 --entdict $edict_file --reldict $rdict_file --testonly $R --score $S
