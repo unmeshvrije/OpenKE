e62c8dd0f9f887a5f4bbdb3cae8cd7e7e14be0be
Dovydas Vadisius <vadisius.dovydas@gmail.com>
2022-10-30 15:44:53 +0100

    generalize subgraph and id lookup for different databases

--- lookup.py
+++ lookup.py
@@ -3,19 +3,20 @@ import pickle
 import argparse
 
 def parse_args():
-    parser = argparse.ArgumentParser(description = 'Create files that translate fb15k237 subgraph ids to entity or relation names.')
+    parser = argparse.ArgumentParser(description = 'Create files that translate subgraph ids to entity or relation names.')
+    parser.add_argument('-db', dest = 'db', type = str, default = 'fb15k237')
     parser.add_argument('--idfile', dest = 'idfile', type = str, default = '/var/scratch/dvs254/kbs/fb15k237-id-to-entity.tsv', help = 'File containing existing subgraph id to entity data')
     parser.add_argument('--savedir', dest = 'save_dir', type = str, default = '/var/scratch/dvs254/kbs/', help = 'Directory in which id to entity and id to relation files will be created')
     return parser.parse_args()
 
 args = parse_args()
 
-efile = "benchmarks/fb15k237/entity2id.txt"
-
-rfile = "benchmarks/fb15k237/relation2id.txt"
-
+db = args.db
 idfile = args.idfile
 save_dir = args.save_dir
+
+efile = "benchmarks/" + db + "/entity2id.txt"
+rfile = "benchmarks/" + db + "/relation2id.txt"
 
 eid_to_fid = {}
 
@@ -25,7 +26,6 @@ with open(idfile, "r") as fin:
     for line in tqdm(lines):
         cols = line.split(maxsplit = 1)
         if len(cols) < 2:
-            #print(line)
             continue
         key = cols[0]
         val = cols[1]
@@ -53,8 +53,8 @@ with open(rfile, "r") as fin:
         key = cols[1]
         id_to_relation[int(key)] = val.rstrip()
 
-with open(save_dir + 'fb15k237-id-to-entity.pkl', 'wb') as fout:
+with open(save_dir + db + '-id-to-entity.pkl', 'wb') as fout:
     pickle.dump(id_to_entity, fout, protocol = pickle.HIGHEST_PROTOCOL)
 
-with open(save_dir + 'fb15k237-id-to-relation.pkl', 'wb') as fout:
+with open(save_dir + db + '-id-to-relation.pkl', 'wb') as fout:
     pickle.dump(id_to_relation, fout, protocol = pickle.HIGHEST_PROTOCOL)
