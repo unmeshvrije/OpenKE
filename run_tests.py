e069ac97e05efbe53e61513090eec154501d51c6
Dovydas Vadisius <vadisius.dovydas@gmail.com>
2023-04-16 20:30:43 +0200

    run_tests.py: save results into a pickle file

--- run_tests.py
+++ run_tests.py
@@ -1,16 +1,17 @@
 import argparse
 import math
+import pickle
 import subprocess
 
 def parse_args():
-    parser = argparse.ArgumentParser(description = 'Run the experiments and extract the results in a format suitable for constructing tables.')
+    parser = argparse.ArgumentParser(description = 'Run the experiments and extract the results in a format suitable for constructing latex tables.')
     parser.add_argument('-db', dest = 'db', type = str, default = 'fb15k237')
-    parser.add_argument('--model', dest = 'model', type = str, default = 'transe')
     parser.add_argument('-r', dest = 'r', type = int, default = '-1', help = 'Number of records to test')
-    parser.add_argument('--metric', dest = 'metric', type = str, default = 'Recall', help = 'The metric of interest, either "Recall" or "Reduction"')
+    parser.add_argument('--metric', dest = 'metric', type = str, default = 'recall', help = 'The metric of interest, either "recall" or "reduction"')
     return parser.parse_args()
 
 TEST_FILE_PATH = "test-subgraphs.sh"
+DATA_DIR_PATH = "results/data/"
 SUBGRAPH_COUNT = {          # used for k = 10% option
     "fb15k237-star": 7694,
     "fb15k237-diamond": 232216,
@@ -32,7 +33,7 @@ SUBGRAPH_COUNT = {          # used for k = 10% option
 #    r - number of records to test, usually 1000 or -1 (all the records)
 #    k - threshold value, usually 10, -1 (dynamic k) or -2 (dynamic threshold)
 #    s - score function. one of "avg", "kl", "nn"
-#    metric - the metric of interest, either "Recall" or "Reduction"/"Red"
+#    metric - the metric of interest, either "recall" or "reduction"
 #    subgraph_type - "star" or "diamond"
 #    max_time - maximum permitted time per task in format "hh:mm:ss"
 # Note: all arguments must be given as strings
@@ -72,7 +73,7 @@ def run_test(test_file_path, database, model, r, k, s, metric = "Recall", subgra
     else:
         return str(round(float(red_H), 2)), str(round(float(red_T), 2))
 
-# Constructs a cell with particular subgraph type for the latex table
+# Generates a small portions of results with particular subgraph type and k value
 def construct_model_cell_results(database, model, r, k, metric, subgraph_type):
     result_avg = run_test(TEST_FILE_PATH, database, model, r, k, "avg", metric, subgraph_type)
     result_kl = run_test(TEST_FILE_PATH, database, model, r, k, "kl", metric, subgraph_type)
@@ -92,7 +93,7 @@ def construct_model_cell_results(database, model, r, k, metric, subgraph_type):
     }
     return experiment_results
 
-# Constructs a portion of a line with particular subgraph type for the latex table
+# Generates a portion of results with a particular subgraph type
 def construct_model_subgraph_type_results(database, model, r, metric, subgraph_type):
     proc = subprocess.Popen("rm ../../../../var/scratch/dvs254/OpenKE-results/" + database + "/subgraphs/" + database + "-" + model + "-subgraphs-tau-10.pkl", stdout = subprocess.PIPE, shell = True)
     proc = subprocess.Popen("prun -v -np 1 -t 00:15:00 -native '-C gpunode --gres=gpu:1' ./step2-create-subgraphs.sh /var/scratch/dvs254/OpenKE-results/ " + model + " " + database + " " + subgraph_type, stdout = subprocess.PIPE, shell = True)
@@ -106,7 +107,7 @@ def construct_model_subgraph_type_results(database, model, r, metric, subgraph_t
     }
     return experiment_results
 
-# Constructs a line of data for the latex table
+# Generates experiment results for a particular database and its model
 def construct_model_results(database, model, r, metric):
     r = str(r)
     experiment_results = dict()
@@ -114,50 +115,18 @@ def construct_model_results(database, model, r, metric):
     experiment_results["diamond"] = construct_model_subgraph_type_results(database, model, r, metric, "diamond")
     return experiment_results
 
-def generate_latex_line(database, model, r, metric):
-    table_line_dict = dict()
-    table_line_dict["star"] = dict()
-    table_line_dict["diamond"] = dict()
-    experiment_results = construct_model_results(database, model, r, metric)
-    for subgraph_type in ["star", "diamond"]:
-        for end_type in ["H", "T"]:
-            type_str = metric + "(H)" if end_type == "H" else metric + "(T)"
-            table_line_dict[subgraph_type][end_type] = "& " + type_str + f"$\\{subgraph_type}$ & "
-            data_string = ""
-            for k in ["10", "10%", "-1", "-2"]:
-                for s in ["avg", "kl", "nn"]:
-                    data_string += str(experiment_results[subgraph_type][k][s][end_type]) + " &"
-            table_line_dict[subgraph_type][end_type] += data_string[:-1] + "\\\\"
-
-    return f"""\\multirow{{4}}{{*}}{{\\rotatebox{{90}}{{\\{model}}}}}
-          {table_line_dict['star']['H']}
-          {table_line_dict['star']['T']}
-          {table_line_dict['diamond']['H']}
-          {table_line_dict['diamond']['T']}
-          """
-
-def generate_latex_table(database, r, metric):
-    print("\\begingroup")
-    print("\\setlength{\\tabcolsep}{6pt} % Default value: 6pt")
-
-    print("\\begin{tabular}{p{0.3em} p{4.5em} || ccc | ccc | ccc | ccc}")
-    print("& \\multirow{2}{*}{\\em K} & \\multicolumn{3}{c}{10} & \\multicolumn{3}{c}{10\\%} & \\multicolumn{3}{c}{$Dyn$} & \\multicolumn{3}{c} {$Dyn^T$}\\\\")
-    print("& & a & d & n & a & d & n & a & d & n & a & d & n \\\\")
-    print("\\cline{2-14}")
-    print(f"{generate_latex_line(database, 'transe', r, metric)}")
-    print("\\cline{2-14}")
-    print(f"{generate_latex_line(database, 'hole', r, metric)}")
-    print("\\cline{2-14}")
-    print(f"{generate_latex_line(database, 'rotate', r, metric)}")
-    print("\\cline{2-14}")
-    print(f"{generate_latex_line(database, 'distmult', r, metric)}")
-    print("\\cline{2-14}")
-    print(f"{generate_latex_line(database, 'complex', r, metric)}")
-    print("\\end{tabular}")
-    print("\\endgroup")
-
+# Generates experiment results for a particular database
+def construct_results(database, r, metric):
+    experiment_results = dict()
+    experiment_results["transe"] = construct_model_results(database, "transe", r, metric)
+    experiment_results["hole"] = construct_model_results(database, "hole", r, metric)
+    experiment_results["rotate"] = construct_model_results(database, "rotate", r, metric)
+    experiment_results["distmult"] = construct_model_results(database, "distmult", r, metric)
+    experiment_results["complex"] = construct_model_results(database, "complex", r, metric)
+    return experiment_results
 
 args = parse_args()
 
-#print(construct_model_results(args.db, args.model, args.r, args.metric))
-generate_latex_table(args.db, args.r, args.metric)
+#generate_latex_table("dbpedia50", 1000, "Recall")
+with open(DATA_DIR_PATH + args.db + '-r' + str(args.r) + '-' + args.metric + '-results.pkl', 'wb') as fout:
+    pickle.dump(construct_results(args.db, args.r, args.metric), fout, protocol = pickle.HIGHEST_PROTOCOL)
