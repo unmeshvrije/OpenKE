7d18d16d6d14e12d377afcc6cc5c3c2d73f14abc
Dovydas Vadisius <vadisius.dovydas@gmail.com>
2023-04-24 17:14:20 +0200

    run_tests.py: save all results (instead of a desired metric)

--- run_tests.py
+++ run_tests.py
@@ -7,7 +7,6 @@ def parse_args():
     parser = argparse.ArgumentParser(description = 'Run the experiments and extract the results in a format suitable for constructing latex tables.')
     parser.add_argument('-db', dest = 'db', type = str, default = 'fb15k237')
     parser.add_argument('-r', dest = 'r', type = int, default = '-1', help = 'Number of records to test')
-    parser.add_argument('--metric', dest = 'metric', type = str, default = 'recall', help = 'The metric of interest, either "recall" or "reduction"')
     return parser.parse_args()
 
 TEST_FILE_PATH = "test-subgraphs.sh"
@@ -49,17 +48,15 @@ def run_test(test_file_path, database, model, r, k, s, subgraph_type = "star", m
     proc = subprocess.Popen("prun -v -np 1 -t " + max_time + " -native '-C gpunode --gres=gpu:1' " + test_file_path + " -m " + model + " -d " + database + " -type " + subgraph_type + " -r " + r + " -k " + k + " -s " + s, stdout = subprocess.PIPE, shell = True)
     return proc
 
-# Processes the result output and extracts the requested metrics
+# Processes the result output and extracts the recall and reduction metrics
 #
 # Arguments:
 #    proc - process that run the test in run_test() function
-#    metric - the metric of interest, either "recall" or "reduction"
 #
 # Results:
-#    1st - reduction/recall (based on the 'metric' argument) value for Head (H)
-#    2nd - reduction/recall value for Tail (T)
-# In a case of unexpected behavior or a time limit the returned tuple is (-1, -1)
-def process_results(result_proc, metric = "recall"):
+#    result dictionary consisting of recall_H, recall_T, red_H, red_T, runtime
+# In a case of unexpected behavior or a time limit the returned tuple is (-1, -1, -1, -1, -1)
+def process_results(result_proc):
     output = result_proc.stdout.readlines()
 
     recall_H = recall_T = red_H = red_T = runtime = 0
@@ -75,69 +72,61 @@ def process_results(result_proc, metric = "recall"):
             red_H = line[14:-1]
         elif line.startswith('%Red (T)'):
             red_T = line[14:-1]
+        elif line.startswith('Runtime'):
+            runtime = line[10:-2]
 
     # Check for undefined behavior
     if len(output) < 2:
-        recall_H = recall_T = red_H = red_T = -1
+        recall_H = recall_T = red_H = red_T = runtime = -1
 
-    if metric.lower() == "recall":
-        return str(round(float(recall_H), 2)), str(round(float(recall_T), 2))
-    else:
-        return str(round(float(red_H), 2)), str(round(float(red_T), 2))
+    results = dict()
+    results["recall_H"] = str(round(float(recall_H), 2))
+    results["recall_T"] = str(round(float(recall_T), 2))
+    results["red_H"] = str(round(float(red_H), 2))
+    results["red_T"] = str(round(float(red_T), 2))
+    results["runtime"] = str(round(float(runtime)))
+    return results
 
 # Generates a small portions of results with particular subgraph type and k value
-def construct_model_cell_results(database, model, r, k, metric, subgraph_type):
+def construct_model_cell_results(database, model, r, k, subgraph_type):
     result_avg_proc = run_test(TEST_FILE_PATH, database, model, r, k, "avg", subgraph_type)
     result_kl_proc = run_test(TEST_FILE_PATH, database, model, r, k, "kl", subgraph_type)
     result_nn_proc = run_test(TEST_FILE_PATH, database, model, r, k, "nn", subgraph_type)
-    result_avg = process_results(result_avg_proc, metric)
-    result_kl = process_results(result_kl_proc, metric)
-    result_nn = process_results(result_nn_proc, metric)
     experiment_results = dict()
-    experiment_results["avg"] = {
-        "H": result_avg[0],
-        "T": result_avg[1]
-    }
-    experiment_results["kl"] = {
-        "H": result_kl[0],
-        "T": result_kl[1]
-    }
-    experiment_results["nn"] = {
-        "H": result_nn[0],
-        "T": result_nn[1]
-    }
+    experiment_results["avg"] = process_results(result_avg_proc)
+    experiment_results["kl"] = process_results(result_kl_proc)
+    experiment_results["nn"] = process_results(result_nn_proc)
     return experiment_results
 
 # Generates a portion of results with a particular subgraph type
-def construct_model_subgraph_type_results(database, model, r, metric, subgraph_type):
+def construct_model_subgraph_type_results(database, model, r, subgraph_type):
     experiment_results = {
-        "10": construct_model_cell_results(database, model, r, "10", metric, subgraph_type),
-        "10%": construct_model_cell_results(database, model, r, "10%", metric, subgraph_type),
-        "-1": construct_model_cell_results(database, model, r, "-1", metric, subgraph_type),
-        "-2": construct_model_cell_results(database, model, r, "-2", metric, subgraph_type)
+        "10": construct_model_cell_results(database, model, r, "10", subgraph_type),
+        "10%": construct_model_cell_results(database, model, r, "10%", subgraph_type),
+        "-1": construct_model_cell_results(database, model, r, "-1", subgraph_type),
+        "-2": construct_model_cell_results(database, model, r, "-2", subgraph_type)
     }
     return experiment_results
 
 # Generates experiment results for a particular database and its model
-def construct_model_results(database, model, r, metric):
+def construct_model_results(database, model, r):
     r = str(r)
     experiment_results = dict()
-    experiment_results["star"] = construct_model_subgraph_type_results(database, model, r, metric, "star")
-    experiment_results["diamond"] = construct_model_subgraph_type_results(database, model, r, metric, "diamond")
+    experiment_results["star"] = construct_model_subgraph_type_results(database, model, r, "star")
+    experiment_results["diamond"] = construct_model_subgraph_type_results(database, model, r, "diamond")
     return experiment_results
 
 # Generates experiment results for a particular database
-def construct_results(database, r, metric):
+def construct_results(database, r):
     experiment_results = dict()
-    experiment_results["transe"] = construct_model_results(database, "transe", r, metric)
-    experiment_results["hole"] = construct_model_results(database, "hole", r, metric)
-    experiment_results["rotate"] = construct_model_results(database, "rotate", r, metric)
-    experiment_results["distmult"] = construct_model_results(database, "distmult", r, metric)
-    experiment_results["complex"] = construct_model_results(database, "complex", r, metric)
+    experiment_results["transe"] = construct_model_results(database, "transe", r)
+    experiment_results["hole"] = construct_model_results(database, "hole", r)
+    experiment_results["rotate"] = construct_model_results(database, "rotate", r)
+    experiment_results["distmult"] = construct_model_results(database, "distmult", r)
+    experiment_results["complex"] = construct_model_results(database, "complex", r)
     return experiment_results
 
 args = parse_args()
 
-#generate_latex_table("dbpedia50", 1000, "Recall")
-with open(DATA_DIR_PATH + args.db + '-r' + str(args.r) + '-' + args.metric + '-results.pkl', 'wb') as fout:
-    pickle.dump(construct_results(args.db, args.r, args.metric), fout, protocol = pickle.HIGHEST_PROTOCOL)
+with open(DATA_DIR_PATH + args.db + '-r' + str(args.r) + '-results.pkl', 'wb') as fout:
+    pickle.dump(construct_results(args.db, args.r), fout, protocol = pickle.HIGHEST_PROTOCOL)
