caa5643e299fbb759ab8e215ddd9319792aa9894
Dovydas Vadisius <vadisius.dovydas@gmail.com>
2023-08-22 12:47:15 +0200

    generate_single_table.py: add precision option

--- generate_latex_single_table.py
+++ generate_latex_single_table.py
@@ -28,6 +28,8 @@ def generate_latex_line(model, r, metric):
     for subgraph_type in ["star", "diamond"]:
         for end_type in ["H", "T"]:
             type_str = metric + "_" + end_type
+            if metric == "prec":
+                type_str = "precision_" + end_type
             table_line_dict[subgraph_type][end_type] = "& " + metric.capitalize() + "(" + end_type + ")" + f"$\\{subgraph_type}$ & "
             data_string = ""
             for k in ["10", "10%", "-1", "-2"]:
@@ -67,6 +69,8 @@ args = parse_args()
 args.metric = args.metric.lower()
 if args.metric == "reduction":
     args.metric = "red"
+if args.metric == "precision":
+    args.metric = "prec"
 
 with open(DATA_DIR_PATH + args.db + '-r' + str(args.r) + '-results.pkl', 'rb') as fin:
     data = pickle.load(fin)
